c9bd70a086a070756a09ff29667d62dbd292f20e
Erez <erezfree29@gmail.com>
2021-03-31 13:48:36 -0700

    add show,new,create methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,21 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+  def new
+    @article = Article.new
+  end
+  
+  def create
+    @article.new(title:"new article",body:"my new article")
+    if @article.save
+      redirect @article
+    else
+      render :new  
+    end  
+  end  
 end
