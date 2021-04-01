5ab50d593b9c15aa116e2a157ba32c2d4df09c73
Niko Escobal <niko.escobal@gmail.com>
2021-04-01 21:36:40 +0800

    Updated articles_controller.rb and index.html.erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,13 +6,14 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-  
+
   def new
     @article = Article.new
   end
-  
+
   def create
     @article = Article.new(article_params)
+
     if @article.save
       redirect_to @article
     else
@@ -20,9 +21,24 @@ class ArticlesController < ApplicationController
     end
   end
 
-private
+  def edit
+    @article = Article.find(params[:id])
+  end
 
- def article_params
-  params.require(:article).permit(:title, :body)
- end
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
