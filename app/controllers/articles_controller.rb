bfbb2326321fa5954e5573955c21c04945227c76
Erez <erezfree29@gmail.com>
2021-04-06 10:54:13 -0700

    add authentication,add destroy method for comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def index
     @articles = Article.all
+    @article2 = Article.find(2)
   end
 
   def show
